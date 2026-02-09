df675a1012c0f52ea9911b2f6f0b3e1159ef3a1a
pibeauvain <pierre.beauvain@etu.uca.fr>
2026-02-09 23:42:19 +0100

    add data corresponding to I=50 and fix indices bug in python file

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import numpy as np
 import subprocess
-import sys
+import matplotlib.pyplot as plt
+import scipy.linalg as alg
 
 # define constants
 m = 100
@@ -16,27 +17,35 @@ DOUBLE_C_SIZE = 8
 # domain definition
 
 nu_values = {0.01, 0.03, 0.06, 0.07}
-I_values = {1, 5, 20, 20} 
+I_values = {1, 5, 20, 50} 
 
 
-data = np.empty((N_x*N_y, len(nu_values)*len(I_values)))
+data = np.empty((N_x*N_y, T*len(nu_values)*len(I_values)))
 
 def generate_data()->None:
+    """
+        ```generate_data``` calls the C executable to compute data and store it as binary files
+    """
     for nu_iter in nu_values:
             for I_iter in I_values:
                 subprocess.run(["./main", str(nu_iter), str(I_iter), f"./data/data_nu{nu_iter}_I{I_iter}.bin"])
 
 
 def import_data(from_stdout:bool=True)->None:
+    """
+        ```import_data``` imports data either on-the-fly from the executable stdout or from binary files
+    """
+
     # compute data and directly imported but is not stored
     if from_stdout:
         for i, nu_iter in enumerate(nu_values):
             for j, I_iter in enumerate(I_values):
                 output = subprocess.run(["./main", str(nu_iter), str(I_iter)], capture_output=True)
-                stride = i*j+j
+                stride = i*len(I_values)*T+j
                 for t in range(T):
-                    data[:, stride*t:stride*(t+1)] += np.frombuffer(output.stdout)[t*SIZE: (t+1)*SIZE].reshape(SIZE, 1)
-    
+                    data[:, stride+t: stride+t+1] = np.frombuffer(output.stdout)[t*SIZE: (t+1)*SIZE].reshape(SIZE, 1)
+
+    # import data from binary files
     else:
         for i, nu_iter in enumerate(nu_values):
             for j, I_iter in enumerate(I_values):
@@ -46,9 +55,14 @@ def import_data(from_stdout:bool=True)->None:
                         data[:, stride*t:stride*(t+1)] += np.frombuffer(f.read(SIZE*DOUBLE_C_SIZE)).reshape(SIZE, 1)
 
 
+
 if __name__ == "__main__":
     #generate_data()
     import_data(False)
     print(data)
+    U, s, V= alg.svd(data)
+    print(s)
+    plt.plot(s)
+    plt.show()
 
 
