b7f2a6e145617af381bd5e5a2a06937ba548073c
pibeauvain <pierre.beauvain@etu.uca.fr>
2026-02-19 17:46:09 +0100

    embed model tracking via MLFlow

--- GPR/GPR_RBF.py
+++ GPR/GPR_RBF.py
@@ -1,49 +1,73 @@
 import sklearn.gaussian_process as gp
 import numpy as np
 import scipy.linalg as linalg
+import mlflow.pyfunc
+from typing import Tuple, Union, Dict
 
 
-class GPR_RBF:
-    def __init__(self, theta:int|np.ndarray=1):
-        self.theta = theta
+class GPR_RBF(mlflow.pyfunc.PythonModel):
+    def __init__(self, theta:Union[np.ndarray, None]=None, sigma:int=0):
+        if theta is None:
+            self.theta = np.ones((1,3))
+        else:
+            self.theta = theta
         self.kernel = gp.kernels.RBF(self.theta)
+        self.sigma = sigma
 
 
     def fit(
             self,
             X_obs:np.ndarray,
             y:np.ndarray,
-            sigma:float=0
+            Uk:np.ndarray
             )->None:
         n_rows = X_obs.shape[0]
+        self.Uk = Uk
     
         # kernel matrix
         self.X_obs = X_obs
-        self.K = np.eye(n_rows)*sigma+self.kernel(self.X_obs, self.X_obs)
+        self.K = np.eye(n_rows)*self.sigma+self.kernel(self.X_obs, self.X_obs)
         c, low = linalg.cho_factor(self.K)
 
         # matrix product K^{-1} by y
         self.invK_y = linalg.cho_solve((c, low), y)
 
 
+
+    def predict_coeff(
+            self,
+            model_input:np.ndarray
+            )->Tuple[np.ndarray, np.ndarray]:
+        
+        mean = self.kernel(model_input, self.X_obs)@self.invK_y
+        cov = self.kernel(model_input, model_input) - \
+            self.kernel(model_input, self.X_obs)@linalg.solve(self.K, self.kernel(self.X_obs, model_input))
+        return mean, cov
+    
     def predict(
             self,
-            x:np.ndarray,
-            return_cov:bool=False
-            )->tuple[np.ndarray, np.ndarray]|np.ndarray:
+            model_input:np.ndarray
+        )->Tuple[np.ndarray, np.ndarray, np.ndarray]:
         
-        mean = self.kernel(x, self.X_obs)@self.invK_y
-        if return_cov:
-            cov = self.kernel(x, x) - \
-                self.kernel(x, self.X_obs)@linalg.solve(self.K, self.kernel(self.X_obs, x))
-            return mean, cov
-        return mean
+        m, s = self.predict_coeff(model_input)
+
+        pred_coeff_upper95 = m + 2*np.reshape(np.sqrt(np.maximum(np.diag(s), 0)), (len(m),1))
+        pred_coeff_lower95 = m - 2*np.reshape(np.sqrt(np.maximum(np.diag(s), 0)), (len(m),1))
+        pred = self.Uk@m.T
+
+        # since coefficients are negative, temperature predictions bounds are switched
+        pred_temperature_lower95 = self.Uk@pred_coeff_upper95.T
+        pred_temperature_upper95 = self.Uk@pred_coeff_lower95.T
+
+        return pred, pred_temperature_lower95, pred_temperature_upper95
+
     
-    def score(self, X_input:np.ndarray, y:np.ndarray)->dict[str, float]:
-        prediction = self.predict(X_input)
+    def score(self, X_input:np.ndarray, y:np.ndarray)->Dict[str, float]:
+        prediction, _ = self.predict_coeff(X_input)
         y_len = len(y)
 
         return {
             "MAE":np.sum(np.abs(prediction-y))/y_len,
             "MSE":np.sum((prediction-y)**2)/y_len
         }
+    
--- __main__.py
+++ __main__.py
@@ -1,107 +1,27 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import DoE.simulation_data as sd
-import scipy.linalg as alg
-import scipy.sparse as sp
+import mlflow
+import pickle
+import os
 
-from GPR.GPR_RBF import GPR_RBF
-from GPR.likelihood import maxlogLikelihood
+# flag whether to save or not the current model
+SAVE_MODEL_FLAG = False
 
-# parameters
-DOUBLE_C_SIZE = 8
+# flag whether to write a pickle file
+WRITE_PICKLE_FLAG = True
 
-# define constants
-m = 100
-n = 100
-T = 20
+MODEL_VERSION_ID = "m-cf5a090b2c474171afd2c02d9d1b7f68"
+MODEL_PATH = f"mlruns/1/models/{MODEL_VERSION_ID}/artifacts"
 
-N_x = n+2
-N_y = m+2
-SIZE = N_x*N_y
+if SAVE_MODEL_FLAG:
+    mlflow.set_experiment("Basic Model From Code")
 
-# domain definition
-nu_values = [0.01, 0.03, 0.06, 0.07]
-I_values = [1, 5, 20, 50]
-t_values = list(range(0, T, 1))
+    model_info = mlflow.pyfunc.log_model(
+        python_model="model.py",  # Path to your script
+        name="gpr_model"
+    )
 
+loaded_pyfunc_model = mlflow.pyfunc.load_model(MODEL_PATH)
 
-A = np.zeros((N_x*N_y, T*len(nu_values)*len(I_values)))
-
-def get_reduced_A(
-    A:np.ndarray, 
-    threshold:float=0.99, 
-    return_Uk_Sk_Vk:bool=False
-    )->np.ndarray|tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """
-        ```get_reduced_A``` returns either the reduced form Ak of A with ```threshold```% of variance or the correponding
-        reduced decomposition (Uk, Sk, Dk) triplet using the SVD decomposition 
-    """
-
-    U, s, V= alg.svd(A)
-    variance_percentage = np.cumsum(s)/np.sum(s)
-    i = 0
-    while i < len(variance_percentage) and variance_percentage[i]<threshold:
-        i+=1
-    S = sp.diags_array(s[:i+1])
-    if return_Uk_Sk_Vk:
-        return U[:,:i+1], S, V[:i+1,:]
-    Ak = U@S@V
-    return Ak
-
-
-def generate_example(
-        gpr:GPR_RBF
-    ):
-    m, s = gpr.predict(sd.get_params_array([0.06], [21], t_values), return_cov=True)
-    pred_coeff_upper95 = m + 2*np.reshape(np.sqrt(np.maximum(np.diag(s), 0)), (len(m),1))
-    pred_coeff_lower95 = m - 2*np.reshape(np.sqrt(np.maximum(np.diag(s), 0)), (len(m),1))
-    pred = Uk@m.T
-
-    # since coefficients are negative, temperature predictions bounds are switched
-    pred_temperature_lower95 = Uk@pred_coeff_upper95.T
-    pred_temperature_upper95 = Uk@pred_coeff_lower95.T
-
-    return pred, pred_temperature_lower95, pred_temperature_upper95
-
-def plot_3D(
-        pred:np.ndarray,
-        pred_temperature_lower95:np.ndarray,
-        pred_temperature_upper95:np.ndarray,
-        )->None:
-    for i in range(T):    
-
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-
-        # Plot a basic wireframe.
-        xv, yv = np.meshgrid(np.linspace(0, 1, 102), np.linspace(0, 1, 102), indexing='ij')
-        ax.plot_wireframe(xv, yv, np.reshape(pred_temperature_lower95[:, i], (102, 102)), rstride=10, cstride=10, alpha=0.3, label="95% confidence interval lower bound")
-        ax.plot_wireframe(xv, yv, np.reshape(pred_temperature_upper95[:, i], (102, 102)), rstride=10, cstride=10, color="r", alpha=0.3, label="95% confidence interval upper bound")
-        ax.plot_surface(xv, yv, np.reshape(pred[:, i], (102, 102)), alpha=0.15, color="g", label="mean prediction")
-        
-        ax.set_xlabel(r'$x$')
-        ax.set_ylabel(r'$y$')
-        ax.set_zlabel(r'temperature ($\degree C$)')
-        ax.legend()
-        plt.show()
-    return None
-
-
-if __name__ == "__main__":
-    #sd.generate_data(nu_values, I_values)
-    sd.import_data(A, nu_values, I_values, from_stdout=True)
-    Uk, Sk, Vk = get_reduced_A(A, return_Uk_Sk_Vk=True)
-
-    a = A.T@Uk
-    params_array = sd.get_params_array(nu_values, I_values, t_values)
-    
-    sigma = 1e-5
-    thetas, _ = maxlogLikelihood(params_array, a, sigma=sigma, verbose=False)
-    gpr = GPR_RBF(thetas)
-
-    gpr.fit(params_array, a, sigma=sigma)
-    print(gpr.score(params_array, a))
-    
-    plot_3D(*generate_example(gpr))
-    
-    
+if WRITE_PICKLE_FLAG:
+    PICKLE_PATH = os.path.join("models", "model.pkl")
+    with open(PICKLE_PATH, "wb") as file:
+        pickle.dump(loaded_pyfunc_model, file)
